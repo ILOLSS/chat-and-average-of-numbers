5d80d614a395744d430a6fc6b5bb5e44db5e68b4
ILOLSS <iloss@mail.ru>
2023-02-09 23:58:46 +0300

    some refactoring

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://chat-and-average-of-numbers-bgotxg306-ilolss.vercel.app/average" },
+          { key: "Access-Control-Allow-Origin", value: "https://chat-and-average-of-numbers.vercel.app" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,POST" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
