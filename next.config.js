9bdb78648705bbc1daf393047a90bfaed0d950de
ILOLSS <iloss@mail.ru>
2023-02-09 23:46:44 +0300

    enable CORS

--- next.config.js
+++ next.config.js
@@ -4,6 +4,20 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://chat-and-average-of-numbers-bgotxg306-ilolss.vercel.app/average" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,POST" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
